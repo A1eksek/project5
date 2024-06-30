388b71c29d394a50a748e5f5dd682b0fa8361f52
a1eksek <alexey20080705@gmail.com>
2024-06-30 17:56:13 +0400

    add file init commit

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-# This is a sample Python script.
+"public(обычный)"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def print_date(self):
+#         print(f'{self.name, self.age}')
+#
+# idl = Person("Vasya", 18)
+# idl.print_date()
+# print(idl.name, idl.age)
+
+"protected(защищёныые атрибуты)"
+
+# class Person:
+#     def __init__(self, name, age):
+#         self._name = name #защищенные
+#         self._age = age #защищенные
+#
+#     def print_date(self):
+#         print(f'{self._name, self._age}')
+#
+# idl = Person("Vasya", 18)
+# idl.print_date()
+# print(idl._name, idl._age)
+
+"Protected method(защищённые методы)"
+
+# class Person:
+#     def _vasya_cat_(self): #защищенный метод
+#         print("Васин кот царапает диван")
+#
+#     def print_protected_method(self):
+#         self._vasya_cat_()
+#
+# id1 = Person()
+# id1.print_protected_method()
+# id1._vasya_cat_()
+
+"Private (приватные) атрибуты"
+
+# class BankAccount:
+#     def __init__(self, balance):
+#         self.__balance = balance
+#
+#     def get_balance(self):
+#         return self.__balance
+#
+# id1 = BankAccount(10000)
+# print(id1.get_balance())
+# print(id1.__balance)
+
+"Private (приватные) методы"
+
+# class Person:
+#     def __pin(self):
+#         print("Пароль от почты")
+#
+#     def get_pin(self):
+#         return self.__pin()
+#
+# id1 = Person()
+# id1.get_pin()
+################################################
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
 
+    def get_balance(self):
+        return self.__balance
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+id1 = BankAccount(10000)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(id1.get_balance())
+print(id1.__dict__)
+print(id1._BankAccount__balance)
